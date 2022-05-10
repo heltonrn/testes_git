03f14e03bafc0dc89f28ad242668d81c008e551a
Helton Ribeiro Nunes <heltonrn@gmail.com>
2022-05-09 22:58:11 -0300

    Ajuste classe pessoa

--- TestesGit/src/main/java/dominio/Pessoa.java
+++ TestesGit/src/main/java/dominio/Pessoa.java
@@ -1,7 +1,21 @@
 package dominio;
 
 public class Pessoa {
-  //testes 2
-	//Teste 3
+	private Integer codigo;
+	private String nome;
+	public Integer getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
 
 }
